0b2bb2705d37b8e9ec929f61ec40e2e720f5fcb6
prashant8941 <155156662+prashant8941@users.noreply.github.com>
2026-08-14 02:14:07 +0530

    Time: 2734 ms (7.61%), Space: 42.7 MB (88.15%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,37 +1,52 @@
 class Solution {
-    public  boolean valid(int i , int j , String s ){
-        while( i < j ){
-            if( s.charAt(i) != s.charAt(j)){
-                return false ; 
+    public boolean valid(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
             }
-            i++ ; 
-            j-- ; 
+            i++;
+            j--;
         }
-        return true ; 
+        return true;
     }
-    public  int find(String s , int[]dp , int ind ){
-        if( ind == s.length()){
-            return  0  ; 
+
+    public int find(String s, int[] dp, int ind) {
+        if (ind == s.length()) {
+            return 0;
         }
-        if( dp[ind]!= -1 ){
-            return dp[ind]; 
+        if (dp[ind] != -1) {
+            return dp[ind];
         }
-        int minC = (int)1e9  ; 
-        for(int i = ind ;i <s.length() ;i++){
-            if( valid(ind , i , s )){
-              int  cost = 1+find(s, dp , i+1); 
-                minC = Math.min(minC, cost ); 
+        int minC = (int) 1e9;
+        for (int i = ind; i < s.length(); i++) {
+            if (valid(ind, i, s)) {
+                int cost = 1 + find(s, dp, i + 1);
+                minC = Math.min(minC, cost);
             }
 
         }
-        return dp[ind] =   minC ; 
+        return dp[ind] = minC;
     }
+
     public int minCut(String s) {
-        int n = s.length(); 
-        int[]dp = new int[n]; 
-        Arrays.fill(dp , -1); 
-        return  find( s , dp ,  0)-1;
- 
-        
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 0;
+
+        for (int j = n - 1; j >= 0; j--) {
+
+            int minC = (int) 1e9;
+            for (int i = j ; i < s.length(); i++) {
+                if (valid(j, i, s)) {
+                    int cost = 1 + dp[i + 1];
+                    minC = Math.min(minC, cost);
+                }
+
+            }
+            dp[j] = minC;
+
+        }
+        return dp[0]-1; 
+
     }
 }
