858b7f28c243670ea7fff7975adf0bb624232672
prashant8941 <155156662+prashant8941@users.noreply.github.com>
2026-08-14 02:03:58 +0530

    Time: 2339 ms (24.54%), Space: 42.8 MB (81.81%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public  boolean valid(int i , int j , String s ){
+        while( i < j ){
+            if( s.charAt(i) != s.charAt(j)){
+                return false ; 
+            }
+            i++ ; 
+            j-- ; 
+        }
+        return true ; 
+    }
+    public  int find(String s , int[]dp , int ind ){
+        if( ind == s.length()){
+            return  0  ; 
+        }
+        if( dp[ind]!= -1 ){
+            return dp[ind]; 
+        }
+        int minC = (int)1e9  ; 
+        for(int i = ind ;i <s.length() ;i++){
+            if( valid(ind , i , s )){
+              int  cost = 1+find(s, dp , i+1); 
+                minC = Math.min(minC, cost ); 
+            }
+
+        }
+        return dp[ind] =   minC ; 
+    }
+    public int minCut(String s) {
+        int n = s.length(); 
+        int[]dp = new int[n]; 
+        Arrays.fill(dp , -1); 
+        return  find( s , dp ,  0)-1;
+ 
+        
+    }
+}
