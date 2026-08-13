06b5a77b2b2133adbf8d0cc89ec105b9b9ac8ff5
prashant8941 <155156662+prashant8941@users.noreply.github.com>
2026-08-14 01:06:13 +0530

    Time: 7 ms (96.97%), Space: 65.3 MB (41%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,34 +1,35 @@
 class Solution {
-    public static boolean palind(String s ){
-        int i =  0 ; 
-        int j = s.length()-1 ; 
-        while( i <= j ){
-            if( s.charAt(i) != s.charAt(j))return false ; 
+    public static boolean valid( String s ){
+        int i =  0  ; 
+        int j = s.length()-1; 
+        while(i< j ){
+            if( s.charAt(i) != s.charAt(j)){
+                return false ; 
+            }
             i++ ; 
             j-- ; 
         }
         return true ; 
     }
-    public static void find(List<List<String>>ans , List<String>temp , int ind , String s ){
+    public static void find(List<List<String>>list,List<String>temp ,  String s , int ind ){
         if(ind == s.length()){
-            ans.add(new ArrayList<>(temp)); 
+            list.add(new ArrayList<>(temp)); 
             return ; 
         }
-        for( int i = ind ;i  < s.length() ; i++){
-            String  ss = s.substring(ind , i+1); 
-            if( palind(ss)){
-                temp.add(ss); 
-             find(ans , temp , i+1 , s); 
-            temp.remove(temp.size()-1); 
+        for( int i = ind ; i < s.length()  ; i++){
+            String sub = s.substring(ind , i+1); 
+            if( valid(sub)){
+                temp.add(sub); 
+                find(list , temp , s , i+1); 
+                temp.remove(temp.size()-1); 
             }
- 
         }
     }
     public List<List<String>> partition(String s) {
-        List<List<String>>ans  = new ArrayList<>(); 
+        List<List<String>>list = new ArrayList<>(); 
         List<String>temp = new ArrayList<>(); 
-        find( ans , temp , 0 , s ); 
-        return ans ; 
+        find( list ,temp ,  s   , 0 ); 
+        return list; 
         
     }
 }
