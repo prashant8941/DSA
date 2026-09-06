f2143f6e2c05990e3e519647219d82b11f0293c0
prashant8941 <155156662+prashant8941@users.noreply.github.com>
2026-09-06 21:28:13 +0530

    Time: 385 ms (11.57%), Space: 237.2 MB (11.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,26 @@
 class Solution {
+    public static int find( int[]nums , int[][]dp , int prev , int ind ){
+        if( ind == nums.length){
+        //    if( prev == -1 || nums[ind]> nums[prev])return 1 ; 
+           return  0 ; 
+        }
+        if( dp[ind][prev+1]!= -1 ){
+            return dp[ind][prev+1];
+        }
+        int nt = find( nums , dp , prev , ind+1); 
+        int tk = 0 ; 
+        if( prev == -1 || nums[ind]> nums[prev] ){
+            tk =1+ find( nums , dp , ind , ind+1); 
+        }
+        return dp[ind][prev+1]= Math.max(tk , nt ); 
+    }
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length ; 
-        int[]dp = new int[n]; 
-        Arrays.fill(dp , 1); 
-        for( int i = 0 ;i < n; i++){
-            for( int j = 0 ;j < i ;j++){
-                if( nums[j]< nums[i] ){
-                    dp[i] = Math.max(dp[i] , dp[j]+1); 
-                }
-
-
-            }
-        }
-        int ans = 0 ; 
-        for( int ele : dp){
-            ans = Math.max( ele ,  ans); 
-        }
-        return ans ; 
+        int n  = nums.length ;
+        int[][]dp = new int[n][n+1]; 
+        for( int i = 0 ;i < n ;i++){
+            Arrays.fill(dp[i] , -1); 
+        } 
+        return find(nums , dp , -1 , 0  ); 
         
     }
 }
