ffac334c6c586828f048ff0eef21517b9ef2881e
prashant8941 <155156662+prashant8941@users.noreply.github.com>
2026-09-18 19:15:12 +0530

    Time: 1 ms (99.95%), Space: 94.2 MB (91.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,13 @@
 class Solution {
-    public static int find( int[]prices , int[][]dp , int ind , int buy , int limit ){
-        if( ind == prices.length || limit == 1 ){
-            return 0 ; 
-        }
-        if( dp[ind][buy]!= -1){
-            return dp[ind][buy]; 
-        }
-        int profit = 0 ; 
-        if( buy == 1 ){
-            profit = Math.max( -prices[ind]+find(prices , dp , ind+1 , 0 , limit ) , 
-            find( prices , dp , ind+1 , 1 , limit )); 
-        }else {
-            profit = Math.max( prices[ind]+find(prices , dp , ind+1 , 1 , limit+1) , 
-            find(prices, dp , ind+1 , 0 , limit)); 
-        }
-        return dp[ind][buy]= profit ;
-    }
     public int maxProfit(int[] prices) {
         int n = prices.length ; 
-        int[][]dp = new int[n][2]; 
-        for( int i = 0 ;i < n ;i++){
-            Arrays.fill(dp[i] , -1); 
+        int min = prices[0] ; 
+        int ans = 0 ; 
+        for(int ele : prices){
+            min = Math.min( ele , min ); 
+            ans = Math.max( ele-min , ans ); 
         }
-        return find(prices , dp , 0 , 1 , 0  ); 
+        return ans ; 
         
     }
 }
