fe8f83af6d42d2c5124328f1dc533457a3c53fea
prashant8941 <155156662+prashant8941@users.noreply.github.com>
2026-09-19 16:55:00 +0530

    Time: 1 ms (87.16%), Space: 44.3 MB (20.47%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,20 @@
 class Solution {
-    public static void find(List<List<Integer>>ans , List<Integer>temp , int []nums , int ind ){
-        if( ind ==  nums.length){
+    public static void find( int[]arr , List<List<Integer>>ans , List<Integer>temp , int ind ){
+        if( ind == arr.length ){
             ans.add(new ArrayList<>(temp)); 
             return ; 
         }
-        temp.add(nums[ind]); 
-        find( ans , temp , nums , ind+1); 
+        temp.add(arr[ind]); 
+        find( arr , ans , temp , ind+1); 
         temp.remove(temp.size()-1); 
-        find( ans , temp , nums , ind+1); 
+        find( arr , ans , temp , ind+1); 
+
     }
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length ; 
         List<List<Integer>>ans = new ArrayList<>(); 
         List<Integer>temp = new ArrayList<>(); 
-        find( ans ,temp , nums , 0  ); 
+        find( nums , ans , temp ,  0); 
         return ans ; 
         
     }
