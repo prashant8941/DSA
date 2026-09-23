3e2ee38b7b6b3e358bc71166493ea921f6cfd412
prashant8941 <155156662+prashant8941@users.noreply.github.com>
2026-09-24 01:10:33 +0530

    Time: 1 ms (99.37%), Space: 47.6 MB (25.13%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,30 +1,25 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n = grid.length ; 
-        int m = grid[0].length ; 
-        int sum = 0 ; 
-        boolean []arr = new boolean[n*n+1];
-        int ans[]  = new int[2] ;
-        Arrays.fill( arr , false); 
-        int dup = 0 ;  
-        for( int i = 0 ;i < n ;i++){
-            int temp = 0 ; 
-            for( int j = 0 ;j < m ;j++){
-                int a = grid[i][j]; 
-                if( arr[a]){
-                    dup = a ; 
+        boolean[]arr = new boolean[n*n+1];
+        int[]ans = new int[2]; 
+        int sum = 0 ;  
+        for( int[]ar : grid){
+            for( int j =  0 ; j < ar.length  ; j++){
+                if( arr[ar[j]] == false ){
+                    arr[ar[j]] = true ; 
+                }else{
+                    ans[0] = ar[j]; 
+                    // break ; 
                 }
-                arr[a] = true ; 
-                temp += a; 
-
+                sum+=ar[j]; 
             }
-            sum+= temp ; 
-           
-        } 
-        ans[0] = dup ; 
-        int val = (n*n)*(n*n+1)/2 ; 
-        ans[1] = (val- ( sum -ans[0])); 
-        return ans ; 
+        }
+        int x = n*n; 
+      int ret  = x*(x+1)/2 - (sum-ans[0]); 
+     ans[1] = ret ; 
+     return ans ; 
+        
         
     }
 }
